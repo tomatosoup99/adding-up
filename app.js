d4829cdcd036e54f7a616bd191ec273b2ae4da49
tomatosoup99 <aoitokage@yahoo.co.jp>
2018-09-29 23:03:44 +0900

    first

--- app.js
+++ app.js
@@ -1,1 +1,9 @@
 'use strict';
+const fs = require('fs');
+const readline = require('readline');
+const rs = fs.ReadStream('./popu-pref.csv');
+const r1 = readline.createInterface({'input': rs, 'output':{} });
+r1.on('line', (lineString) => {
+    console.log(lineString);
+});
+r1.resume();
